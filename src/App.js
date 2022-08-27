781e5d4def898cc24b2bfe155a94a952c0c73523
mehran vejdani <mehran.vejdanii@gmail.com>
2022-08-27 18:19:11 +0430

    formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Signin from "./components/signin";
+import Login from "./components/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App-logo">
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Navigate to="/signin" />} />
+        </Routes>
+      </div>
     </div>
   );
 }
